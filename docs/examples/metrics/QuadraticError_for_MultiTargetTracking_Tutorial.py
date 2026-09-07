c4a42a0898be798391529090ccb3f30bee938eec
Rudyard1 <115188735+Rudyard1@users.noreply.github.com>
2026-09-07 12:53:59 +0100

    Retry Docs tests

--- docs/examples/metrics/QuadraticError_for_MultiTargetTracking_Tutorial.py
+++ docs/examples/metrics/QuadraticError_for_MultiTargetTracking_Tutorial.py
@@ -493,7 +493,7 @@ filter_data_dict = {'filter model': 'GMPHD',
                     'updater': extended_kalman_updater}
 
 reward_function = QuadraticInformationGain(
-    num_samples=200,
+    num_samples=250,
     state_dim=4,
     filter_data=filter_data_dict,
     kernel='Gaussian',
